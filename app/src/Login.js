8d15747691d030f4059c86d38057e751d4ca0df1
rudi rughoonundon <rudir@yahoo.com>
2020-06-13 03:51:36 -0700

    try axios for google authentication again.

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
   const history = useHistory();
 
-  function handleGoogleSignInButtonClick () {
-    history.push("/google-signin") ;
+  function handleResponse(response) {
+    console.log("data: " + response.data);
+    console.log("status: " + response.status);
+    console.log("text: " + response.statusText);
+    console.log("headers:" + response.headers);
+    console.log("config:" + response.config);
+  }
+
+  function handleError(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  }
+
+  function handleGoogleSignInButtonClick() {
+    // history.push("/google-signin") ;
+
+    let client_id = '1086335094551-q0vbi5a05o2dghg1nge2ejkg5oqvkilm.apps.googleusercontent.com';
+    let config = {
+      url: 'https://accounts.google.com/o/oauth2/v2/auth',
+      method: 'get',
+      params: {
+        scope: 'email profile openid',
+        response_type: 'token',
+        state: 'random_value_here',
+        redirect_uri: 'https://www.kyru.io/oauth2/google',
+        client_id: client_id,
+      },
+      withCredentials: true
+    };
+    axios(config)
+    .then(handleResponse, handleError)
+    .catch(handleError) ;
   }
 
   function handleRachioSignInButtonClick () {
